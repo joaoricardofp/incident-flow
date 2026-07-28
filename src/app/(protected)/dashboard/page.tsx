8d65f9d87060b91f49da2360f0a13a67020ffc2b
João Ricardo (JRFP) <140343715+joaoricardofp@users.noreply.github.com>
2026-07-28 11:06:23 -0300

    feat(dashboard): implement workspace list view and add sidebar UI components

--- src/app/(protected)/dashboard/page.tsx
+++ src/app/(protected)/dashboard/page.tsx
@@ -1,3 +1,21 @@
-export default function DashboardPage() {
-  return <>Dashboard</>;
+import { getSession } from "@/lib/auth";
+import { WorkspaceCard } from "@/modules/workspace/components/workspace-card";
+import { getWorkspacesByUser } from "@/modules/workspace/queries";
+
+export default async function DashboardPage() {
+  const session = await getSession();
+
+  const workspaces = await getWorkspacesByUser({
+    userId: session?.user.id as string,
+  });
+
+  return (
+    <div className="flex flex-1 flex-col gap-4 p-4">
+      <div className="grid auto-rows-min gap-4 md:grid-cols-3">
+        {workspaces.map((workspace) => (
+          <WorkspaceCard key={workspace.id} {...workspace} />
+        ))}
+      </div>
+    </div>
+  );
 }
